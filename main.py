b8916a854600b8096052a6308875bb74c9defff4
Silasmk <silasmck@t-online.de>
2026-05-22 09:51:54 +0200

    KV-Matrix Display

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-import tkinter as tk
 from tkinter import *
+import numpy as np
 
 root = Tk()
 root.geometry("800x600")
 
 label = Label(root, text="Hello World!")
 label.pack()
+
+size = 4
+kv_matrix = np.zeros((size,size))
+
+for i in kv_matrix:
+    print(i)
+    row = Label(root, text=i)
+    row.pack()
 
 
 root.mainloop()
